54c26bfbb9a95777496f2dec248a062887872456
Hugo Morais <hugo.morais@frontkom.com>
2026-01-06 20:43:49 +0000

    Create Vue app

---
+++ Advanced web development - Book/Animalec/.eslintrc.cjs
@@ -0,0 +1,16 @@
+/* eslint-env node */
+/* RouterVuex Configuration */
+require('@vue/eslint-config-prettier')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
+
---
+++ Advanced web development - Book/Animalec/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Animalec</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
+
